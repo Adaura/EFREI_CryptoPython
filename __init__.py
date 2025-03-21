14e43c5cec0231b5f700d56b85d913a1b0ff7ea5
Adaura <96139981+Adaura@users.noreply.github.com>
2025-03-21 10:15:29 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,36 +1,27 @@
 from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-key = Fernet.generate_key()
+key = b'9QsgaRHRrtV2PF9hcJTwjjRZdTEqUtahTImaeudRaZw='
 f = Fernet(key)
 
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    token = f.encrypt(valeur.encode())
+    return f"Valeur encryptée : {token.decode()}"
 
 @app.route('/decrypt/<string:token>')
 def decryptage(token):
     try:
-        # Convertir le token (string) en bytes
-        token_bytes = token.encode()
-        # Déchiffrer avec le même Fernet (même clé)
-        valeur_decryptee = f.decrypt(token_bytes)
-        # Convertir le résultat en string
+        valeur_decryptee = f.decrypt(token.encode())
         return f"Valeur décryptée : {valeur_decryptee.decode()}"
     except Exception as e:
         return f"Erreur lors du décryptage : {str(e)}", 400
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
